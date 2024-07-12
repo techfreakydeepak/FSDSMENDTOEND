5ae46eee8fe61a174ff83d2ab1a94aa884b19e1e
techfreakydeepak <dpsingh1518@gmail.com>
2024-07-12 13:47:05 +0530

    exception module added

--- src/DimondPricePrediction/logger.py
+++ src/DimondPricePrediction/logger.py
@@ -13,3 +13,6 @@ logging.basicConfig(level=logging.INFO,
                     filename=LOG_FILE_PATH,
                     format="[%(asctime)s] %(lineno)d %(levelname)s - %(message)s",
 )
+
+if __name__ =="__main__":
+    logging.info("This is a test log message")
