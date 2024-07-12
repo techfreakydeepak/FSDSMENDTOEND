12f8cc0619bdfbf88182f59cf15883663b85532b
techfreakydeepak <dpsingh1518@gmail.com>
2024-07-12 13:04:09 +0530

    logger file uploaded

---
+++ src/DimondPricePrediction/logger.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+import logging
+import os
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%y_%M_%S')}.log"
+
+log_path = os.path.join(os.getcwd(), "logs")
+os.makedirs(log_path, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(log_path, LOG_FILE)
+
+logging.basicConfig(level=logging.INFO,
+                    filename=LOG_FILE_PATH,
+                    format="[%(asctime)s] %(lineno)d %(levelname)s - %(message)s",
+)
