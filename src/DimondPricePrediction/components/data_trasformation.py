5b7865b221041178ada93f915f3cd12acb36bfc6
techfreakydeepak <dpsingh1518@gmail.com>
2024-07-12 11:44:06 +0530

    file updated

---
+++ src/DimondPricePrediction/components/data_trasformation.py
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+
+class DataTransformation:
+    def __init__(self):
+        pass
+
+    def get_data_tranformation(self):
+        pass
---
+++ src/DimondPricePrediction/components/model_trainer.py
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+
+class ModelTrainer:
+    def __init__(self):
+        pass
+
+
+    def initate_model_trainig(self):
+        pass
